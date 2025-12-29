094b2dbd5a54902b1865687b9b27d7a9c2832bbc
Buniek <82052285+bun1a-dev@users.noreply.github.com>
2025-12-29 10:37:00 +0100

    Refactor BeamNG RPM LED control for G923

--- beamng-shifting-leds.js
+++ beamng-shifting-leds.js
@@ -1,92 +1,98 @@
 #!/usr/bin/node
 
-// Using some code from https://github.com/d4rk/ac_shifting_leds/blob/main/ac_shifting_leds.js and https://github.com/fuelsoft/out-gauge-cluster/blob/master/OutGaugeCluster/OutGaugeCluster.pde
+// BeamNG → Logitech G923 RPM LEDs
 
 const hid = require('node-hid');
 const dgram = require('dgram');
-const { abort, exit } = require('process');
+const { exit } = require('process');
 
 const socket = dgram.createSocket('udp4');
 const PORT = 5555;
 const ADDRESS = "127.0.0.1";
 
-var rpm;
-var maxrpm = 7000;
-var g29;
-var firstTime = true;
-var LEDFlashTime = 0;
+// ====== STATE ======
+let rpm = 0;
+let maxrpm = 7000;
+let g923;
 
+let firstTime = true;
+let previousLEDMask = -1;
+let LEDsOn = false;
+let LEDFlashTime = 0;
+
+// ====== INPUT ======
 process.stdin.on('data', data => {
-  maxrpm = Number(data.toString());
-  console.log("The Max RPM is now " + maxrpm);
+  const val = Number(data.toString());
+  if (!isNaN(val) && val > 0) {
+    maxrpm = val;
+    console.log("Max RPM set to:", maxrpm);
+  }
 });
 
-console.log("You can enter the Max RPM of the car you are using at any time by typing the number of RPM in and pressing enter. The default is 7000 RPM");
+console.log("Type Max RPM and press ENTER (default 7000)");
 
+// ====== UDP ======
 socket.bind(PORT, ADDRESS);
 
-if (g29 == undefined) {
-    // Connect to the first Logitech G29.
-  try {
-    g29 = new hid.HID(1133, 49743);
-    console.log("Connected to Logitech G29 wheel");
-  } catch (e) {
-    console.log("Could not open the Logitech G29 wheel. This may be due to insufficent permissions");
-    console.log(e);
-    exit(1);
-  }
+// ====== HID ======
+try {
+  g923 = new hid.HID(0x046d, 0xc266);
+  console.log("Connected to Logitech G923");
+} catch (e) {
+  console.log("Could not open Logitech G923 (permissions?)");
+  console.log(e);
+  exit(1);
 }
 
-socket.on("message", (msg, rinfo) => {
-    if (firstTime == true){
-      console.log("Connected to BeamNG.Drive");
-      firstTime = false;
-    }
-    rpm = new Float32Array(new Uint8Array([msg[16], msg[17], msg[18], msg[19]]).buffer)[0];
-    updateLeds();
+// ====== DATA ======
+socket.on("message", (msg) => {
+  if (firstTime) {
+    console.log("Connected to BeamNG.Drive");
+    firstTime = false;
+  }
+
+  // RPM from BeamNG (little-endian float)
+  rpm = msg.readFloatLE(16);
+  rpm = Math.max(0, Math.min(rpm, maxrpm));
+
+  updateLeds();
 });
 
-function updateLeds(){
-    const rpmFrac = rpm / maxrpm;
+// ====== LED LOGIC ======
+function updateLeds() {
+  const rpmFrac = rpm / maxrpm;
+  const now = Date.now();
 
-    this.hrTime = process.hrtime();
+  // Limiter flash
+  if (rpmFrac > 0.93) {
+    if (now - LEDFlashTime > 100) {
+      flashLEDs();
+    }
+    return;
+  }
 
-    // Convert rpmFrac to an LED range.
-    var LEDMask = 0x0;
-    if (rpmFrac > 0.45) {
-        LEDMask |= 0x1;
-    }
-    if (rpmFrac > 0.55) {
-        LEDMask |= 0x2;
-    }
-    if (rpmFrac > 0.625) {
-        LEDMask |= 0x4;
-    }
-    if (rpmFrac > 0.71) {
-        LEDMask |= 0x8;
-    }
-    if (rpmFrac > 0.85) {
-        LEDMask |= 0x10;
-    }
-    // Flash LEDs
-    if (rpmFrac > 0.93){
-        if (this.hrTime[0] * 1000000 + this.hrTime[1] / 1000 - LEDFlashTime > 100000){
-            flashLEDs();
-        }
-    }
-    if (LEDMask == this.previousLEDMask) {
-        return;
-    }
-    this.previousLEDMask = LEDMask;
-    g29.write([0xf8, 0x12, LEDMask, 0x00, 0x00, 0x00, 0x01])
+  let LEDMask = 0x0;
+
+  if (rpmFrac > 0.45)  LEDMask |= 0x01;
+  if (rpmFrac > 0.55)  LEDMask |= 0x02;
+  if (rpmFrac > 0.625) LEDMask |= 0x04;
+  if (rpmFrac > 0.71)  LEDMask |= 0x08;
+  if (rpmFrac > 0.85)  LEDMask |= 0x10;
+
+  if (LEDMask === previousLEDMask) return;
+
+  previousLEDMask = LEDMask;
+  g923.write([0xf8, 0x12, LEDMask & 0x1F, 0x00, 0x00, 0x00, 0x01]);
 }
 
+// ====== FLASH ======
 function flashLEDs() {
-    if (this.LEDsOn) {
-      g29.write([0xf8, 0x12, 31, 0x00, 0x00, 0x00, 0x01])
-    } else {
-      g29.write([0xf8, 0x12, 0, 0x00, 0x00, 0x00, 0x01])
-    }
-    this.LEDsOn = !this.LEDsOn;
-    LEDFlashTime = this.hrTime[0] * 1000000 + this.hrTime[1] / 1000;
+  if (LEDsOn) {
+    g923.write([0xf8, 0x12, 0x1F, 0x00, 0x00, 0x00, 0x01]);
+  } else {
+    g923.write([0xf8, 0x12, 0x00, 0x00, 0x00, 0x00, 0x01]);
+  }
+
+  LEDsOn = !LEDsOn;
+  LEDFlashTime = Date.now();
 }
